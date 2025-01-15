c8c7ce540cf2002d3a5e7202f1abc2dd6c52a9a4
Vivek Addagatla <2024.vivekp@isu.ac.in>
2025-01-15 20:38:10 +0530

    Added

--- Q23.py
+++ Q23.py
@@ -1,4 +1,7 @@
 numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
 target = int(input("Enter the number to count: "))
-count = sum(1 for num in numbers if num == target)
-print(f"{target} appears {count} times.")
+count = 0
+for char in numbers:
+    if(char==target):
+        count+=1
+print("Count is ",count)
