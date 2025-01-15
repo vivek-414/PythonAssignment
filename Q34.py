92353afcbfcd7789da3ee08e2a86621ec8ffcf61
Vivek Addagatla <2024.vivekp@isu.ac.in>
2025-01-15 22:26:52 +0530

    Changes

--- Q34.py
+++ Q34.py
@@ -1,3 +1,10 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 start = int(input("Enter the start of the range: "))
 end = int(input("Enter the end of the range: "))
 print(f"Prime numbers between {start} and {end}:")
